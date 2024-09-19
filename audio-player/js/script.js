46b1b38e5413eed9c7b1ae5b85883c06912f37fc
Evgeniy <defender35185@gmail.com>
2024-09-19 18:56:07 +0300

    add the main application logic

--- audio-player/js/script.js
+++ audio-player/js/script.js
@@ -1,47 +1,29 @@
- const rangeTime = document.getElementById('time');
- const rangeVolume = document.getElementById('volume');
-
-    function updateBackground(rangeInput) {
-        const value = rangeInput.value;
-        const min = rangeInput.min || 0;
-        const max = rangeInput.max || 100;
-        const percentage = (value - min) / (max - min) * 100;
-        rangeInput.style.setProperty('--value', percentage + '%');
-    }
-
-    rangeTime.addEventListener('input', () => {
-        updateBackground(rangeTime);
-    });
-    rangeVolume.addEventListener('input', () => {
-        updateBackground(rangeVolume);
-    });
-    updateBackground(rangeTime);
-    updateBackground(rangeVolume);
-
-const data = [
-    {
-        audio: 'assets/audio/beyonce.mp3',
-        img: '/assets/img/lemonade.png',
-        title: 'Beyonce',
-        subtitle: "Don't Hurt Yourself"
-    },
-    {
-        audio: 'assets/audio/dontstartnow.mp3',
-        img: '/assets/img/dontstartnow.png',
-        title: 'Dua Lipa',
-        subtitle: "Don't Start Now"
-    }
-];
-let currentAudio = 0;
-const audioElem = document.querySelector('audio');
+const rangeTime = document.getElementById('time');
+const rangeVolume = document.getElementById('volume');
+const audioElem = document.getElementById('audio');
 const playPauseBtn = document.querySelector('.player__play');
 const prevBtn = document.querySelector('.player__prev');
 const nextBtn = document.querySelector('.player__next');
 const images = document.querySelectorAll('body, .player__top');
 const title = document.querySelector('.player__title');
 const subtitle = document.querySelector('.player__subtitle');
-
-audioElem.setAttribute('src', data[currentAudio].audio);
+let currentAudio = 0;
+let duration
+let refreshTime
+const data = [
+    {
+        audio: 'assets/audio/beyonce.mp3',
+        img: 'assets/img/lemonade.png',
+        title: 'Beyonce',
+        subtitle: "Don't Hurt Yourself"
+    },
+    {
+        audio: 'assets/audio/dontstartnow.mp3',
+        img: 'assets/img/dontstartnow.png',
+        title: 'Dua Lipa',
+        subtitle: "Don't Start Now"
+    }
+];
 
 function setAudio(direction = 0) {
     if ((currentAudio + direction) < 0) {
@@ -57,25 +39,68 @@ function setAudio(direction = 0) {
     title.textContent = data[currentAudio].title;
     subtitle.textContent = data[currentAudio].subtitle;
     audioElem.setAttribute('src', data[currentAudio].audio);
-    audioElem.play();
-    playPauseBtn.classList.remove('player_pause');
+    audioElem.load();
 }
 
 playPauseBtn.addEventListener('click', (e) => {
     if (e.currentTarget.classList.contains('player_pause')) {
         e.currentTarget.classList.remove('player_pause');
         audioElem.play();
+        refreshTime = setInterval(refreshTimeRange, 1000);
     } else {
         e.currentTarget.classList.add('player_pause');
         audioElem.pause();
+        clearInterval(refreshTime)
     }
 })
+
 prevBtn.addEventListener('click', () => {
     setAudio(-1);
+    audioElem.play();
+    playPauseBtn.classList.remove('player_pause');
+    refreshTime = setInterval(refreshTimeRange, 1000);
 })
+
 nextBtn.addEventListener('click', () => {
     setAudio(1);
+    audioElem.play();
+    playPauseBtn.classList.remove('player_pause');
+    refreshTime = setInterval(refreshTimeRange, 1000);
 })
 
-console.dir(audioElem)
+function setRange(rangeInput) {
+    rangeInput.min = 0
+    if (rangeInput.id === 'time') {
+        rangeInput.max = duration;
+        rangeInput.style.setProperty('--value', 0 + '%');
+    } else if (rangeInput.id === 'volume') {
+        rangeInput.max = 100;
+        audioElem.volume = 0.5;
+        rangeInput.style.setProperty('--value', 75 + '%');
+    }
+}
 
+rangeTime.addEventListener('input', (e) => {
+    e.target.style.setProperty('--value', (e.target.value / duration) * 100 + '%');
+    audioElem.currentTime = rangeTime.value;
+})
+rangeVolume.addEventListener('input', (e) => {
+    e.target.style.setProperty('--value', e.target.value + '%');
+    audioElem.volume = rangeVolume.value / 100;
+})
+
+function refreshTimeRange() {
+    rangeTime.style.setProperty('--value', (audioElem.currentTime / duration) * 100 + '%');
+    if (audioElem.currentTime === duration) {
+        setAudio(1);
+        audioElem.play();
+    }
+}
+
+setAudio();
+setRange(rangeVolume);
+
+audioElem.addEventListener('loadedmetadata', () => {
+    duration = audioElem.duration
+    setRange(rangeTime);
+})
